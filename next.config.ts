192b9ab8252a17cf1b69100423ff0aa0d85b2396
Gladyson <gladysonss@gmail.com>
2025-12-09 18:01:25 -0300

    chore: Ignore ESLint errors during Next.js builds.

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,9 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [new URL("https://lh3.googleusercontent.com/**")],
   },
